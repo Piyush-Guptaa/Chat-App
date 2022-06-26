8163b3e12ec6c94c08c8bdac15ae00f6f509a0d0
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2022-06-26 16:20:47 +0530

    Update settings.dart

--- lib/src/utils/settings.dart
+++ lib/src/utils/settings.dart
@@ -1,4 +1,4 @@
 /// Define App ID and Token
 const APP_ID = '8d755ef5e0c34a9ba38b3fd18ae5a188';
 const Token =
-    '0068d755ef5e0c34a9ba38b3fd18ae5a188IAAxkLGmTp4bBt55EBdDGXhB0R3I+TxslSnno5aPSd/t6VKa+MwAAAAAEACJVdSDyGy0YgEAAQDHbLRi';
+    '0068d755ef5e0c34a9ba38b3fd18ae5a188IABBtLM0vZxN57n89CuMgh2PauwZzq3P6kO5ey2SZJysw1Ka+MwAAAAAEABGROOeRYu5YgEAAQBEi7li';
