d543f093262e299eeb2d1005a6a75a0b29a79bd1
Piyush Gupta <77025176+Piyush-Guptaa@users.noreply.github.com>
2022-06-26 19:39:03 +0530

    Update home_screen.dart

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:permission_handler/permission_handler.dart';
+
+import 'src/pages/call.dart';
 
 
 class HomeScreen extends StatefulWidget {
@@ -18,8 +22,39 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text('Home'),
       ),
       body: Center(
-        child: Text('+91 ${widget.phoneNumber}'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Welcome to Home Screen'),
+            Text('Phone Number: ${widget.phoneNumber}'),
+            ElevatedButton(
+                                  onPressed: () async {await _handleCameraAndMic(Permission.camera);
+      await _handleCameraAndMic(Permission.microphone);
+      // push video page with given channel name
+      await Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => CallPage(
+            channelName: 'piyush123',
+            role: ClientRole.Broadcaster//_role,
+          ),
+        ),);
+                                    },
+                                  
+                                  child: Row(
+                                    children: [
+                                      Icon(Icons.video_call),
+                                      SizedBox(width: 10),
+                                      Text('start instant meeting'),
+                                    ],
+                                  )),
+          ],
+        ),
       ),
     );
+  }
+    Future<void> _handleCameraAndMic(Permission permission) async {
+    final status = await permission.request();
+    print(status);
   }
 }
